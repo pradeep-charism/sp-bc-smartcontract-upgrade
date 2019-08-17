689647217e812347c9b8f1cb667514b09fe2a855
pradeep-charism <pradeep.charism@gmail.com>
2019-08-17 15:25:44 +0800

    Using open zeplin and delegate pattern for contract upgrade

---
+++ contracts/Counter.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.5.0;
+
+import '@openzeppelin/upgrades/contracts/Initializable.sol';
+
+contract Counter is Initializable {
+
+  uint public counter;
+
+  function increaseCounter() external {
+    // counter = counter + 2;
+    counter = counter + 1;
+  }
+
+  function initialize(uint _counter) public initializer {
+    counter = _counter;
+  }
+
+
+}
